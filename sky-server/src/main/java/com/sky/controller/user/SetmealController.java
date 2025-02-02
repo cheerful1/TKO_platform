3d94f239037532846b71a8e93498ae1f9621e19d
wsj <1547921719@qq.com>
2025-02-02 20:33:58 +0800

    redis缓存的SpringCache

--- sky-server/src/main/java/com/sky/controller/user/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/user/SetmealController.java
@@ -8,6 +8,7 @@ import com.sky.vo.DishItemVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,16 +25,16 @@ public class SetmealController {
     /**
      * 条件查询
      *
-     * @param categoryId
-     * @return
+     * @param categoryId 分类id
+     * @return 套餐列表
      */
     @GetMapping("/list")
     @ApiOperation("根据分类id查询套餐")
+    @Cacheable(value = "setmealCache", key = "#categoryId")
     public Result<List<Setmeal>> list(Long categoryId) {
         Setmeal setmeal = new Setmeal();
         setmeal.setCategoryId(categoryId);
         setmeal.setStatus(StatusConstant.ENABLE);
-
         List<Setmeal> list = setmealService.list(setmeal);
         return Result.success(list);
     }
@@ -41,8 +42,8 @@ public class SetmealController {
     /**
      * 根据套餐id查询包含的菜品列表
      *
-     * @param id
-     * @return
+     * @param id 套餐id
+     * @return 包含的菜品列表
      */
     @GetMapping("/dish/{id}")
     @ApiOperation("根据套餐id查询包含的菜品列表")
---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,8 @@
+package com.sky.controller.user;
+
+/**
+ * @author : wangshanjie
+ * @date : 20:33 2025/2/2
+ */
+public class ShoppingCartController {
+}
